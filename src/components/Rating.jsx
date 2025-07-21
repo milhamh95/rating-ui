10dee7fca978c544ce2c55cd006c42e1f0d7c8c2
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-07-21 21:48:35 +0700

    feat: list

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function Rating() {
     return (
-        <div>
+        <div className="rating-container">
             <h2>Rate Your Experience</h2>
+            <div className="stars">
+                {
+                    [1,2,3,4,5].map((star) => {
+                        return <span key={star}>{"\u2606"}</span>
+                    })
+                }
+            </div>
         </div>
     )
 }
